45d3af8820e3b6fc32ae4f9c418eeb6def416c7c
DK <dkom91@gmail.com>
2020-10-22 19:11:32 +0300

    lesson_7 add some

--- src/main/java/root/lesson_7/Solution.java
+++ src/main/java/root/lesson_7/Solution.java
@@ -3,5 +3,8 @@ package root.lesson_7;
 public class Solution {
     public static void main(String[] args) {
         System.out.println("HELLO");
+        System.out.println("HELLO");
+        System.out.println("HELLO");
+        System.out.println("HELLO");
     }
 }
